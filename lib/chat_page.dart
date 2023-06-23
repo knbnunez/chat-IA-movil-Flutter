953aeb25d7e08364aeddeea20e71881f2c77b321
knbnunez <knbnunez@gmail.com>
2023-06-23 20:59:49 -0300

    Se añadió un backArrow para navegar hacia el home y se componentizó el StateBar

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -93,46 +93,76 @@ class _ChatPageState extends State<ChatPage> {
 
     return Scaffold(
         body: Container(
-            padding: const EdgeInsets.only(top: 45, left: 24, right: 24),
+            padding: const EdgeInsets.only(top: 45, left: 20, right: 20),
             child: Column(children: [
               const StateBar(),
 
+              //  En el de Leo, esto fue modificado para poder encapsular los textos en un container (que terminó siendo un nuevo widget) con estilo de usuario.
               Expanded(
                 child: ListView(
                   children: messages.map((e) => Text(e)).toList()
                 )
               ),
 
-              // ------------ //
-              //   inputText  //
-              // ------------ //
-              Container(
-                width: double.infinity,
-                height: 48,
-                margin: const EdgeInsets.only(bottom: 20),
-                padding: const EdgeInsets.only(left: 15, right: 15),
-                decoration: BoxDecoration(
-                    // color: const Color.fromARGB(31, 3, 146, 255),
-                    borderRadius: BorderRadius.circular(24),
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 1,
-                    )),
-                child: TextField(
-                  controller: textFieldController,
-                  onSubmitted: (value) {
-                    setState(() {
-                      messages.add(value);
-                    });
-                    textFieldController.clear();
-                  },
-                  decoration: const InputDecoration(
-                    border: InputBorder.none,
+              Row(
+                children: [
+                  // ------------ //
+                  //   inputText  //
+                  // ------------ //
+                  Expanded(
+                    child: Container(
+                        // width: double.infinity,
+                        // width: 200,
+                        height: 48,
+                        margin: const EdgeInsets.only(bottom: 20),
+                        padding: const EdgeInsets.only(left: 15, right: 15),
+                        decoration: BoxDecoration(
+                          // color: const Color.fromARGB(31, 3, 146, 255),
+                          borderRadius: BorderRadius.circular(24),
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 1,
+                          )),
+                        child: TextField(
+                          controller: textFieldController,
+                          onSubmitted: (value) {
+                            setState(() {
+                              messages.add(value);
+                            });
+                            textFieldController.clear();
+                          },
+                          decoration: const InputDecoration(
+                            border: InputBorder.none,
+                          ),
+                        ),
+                    ),
                   ),
-                ),
-              )
+
+                  // ------------ //
+                  //  Send Button //
+                  // ------------ //
+                  Container(
+                    margin: const EdgeInsets.only(left: 10, bottom: 20),
+                    decoration: BoxDecoration(
+                        color: const Color.fromRGBO(48, 52, 55, 1),
+                        borderRadius: BorderRadius.circular(50)),
+                    child: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          messages.add(textFieldController.text);
+                        });
+                        textFieldController.clear();
+                      },
+                      icon: const Icon(Icons.send),
+                      color: Colors.white,
+                    )
+                  ),
+                ],
+              ),
             ])
           )
         );
   }
 }
+
+
